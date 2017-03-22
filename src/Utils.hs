269e1557af57d96ca00233412784e5bfff637ebd
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-03-22 19:24:37 +0100

    Upgrade hindent

--- src/Utils.hs
+++ src/Utils.hs
@@ -2,23 +2,17 @@ module Utils where
 
 import Types
 
-yieldMaybe
-    :: (Monad m)
-    => Maybe b -> Proxy a' a () b m ()
+yieldMaybe :: (Monad m) => Maybe b -> Proxy a' a () b m ()
 yieldMaybe Nothing = return ()
 yieldMaybe (Just a) = yield a
 
-fstPicker
-    :: Monad m
-    => Pipe (a, b) a m ()
+fstPicker :: Monad m => Pipe (a, b) a m ()
 fstPicker =
     forever $ do
         (a, _) <- await
         yield a
 
-sndPicker
-    :: Monad m
-    => Pipe (a, b) b m ()
+sndPicker :: Monad m => Pipe (a, b) b m ()
 sndPicker =
     forever $ do
         (_, b) <- await
